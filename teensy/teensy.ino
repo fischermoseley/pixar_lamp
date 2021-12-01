3e805cc71266090e27bffaf225da8342794e1d87
Fischer Moseley <42497969+FischerMoseley@users.noreply.github.com>
2021-12-01 10:22:56 -0500

    update naming conventions to be more readable

--- teensy/teensy.ino
+++ teensy/teensy.ino
@@ -2,31 +2,62 @@
 #include "trajectory.h"
 #include <Encoder.h>
 
-VescUart shoulder;
-VescUart elbow;
+// th1 is the shoulder, th2 is the elbow
 
-VescUart::dataPackage shoulder_telemetry[trajectory_length];
-VescUart::dataPackage elbow_telemetry[trajectory_length];
+VescUart th1_vesc;
+VescUart th2_vesc;
 
-Encoder shoulder_encoder(7,8);
-Encoder elbow_encoder(5,6);
+VescUart::dataPackage th1_telemetry[trajectory_length];
+VescUart::dataPackage th2_telemetry[trajectory_length];
 
-int32_t initial_shoulder_angle = 0;
-int32_t initial_elbow_angle = 0;
-int32_t last_shoulder_angle = 0;
-int32_t last_elbow_angle = 0; 
+Encoder th1_encoder(7,8);
+Encoder th2_encoder(5,6);
 
-int32_t current_shoulder_angle = 0;
-int32_t current_elbow_angle = 0;
+int32_t th1_desired = 0;
+int32_t th2_desired = 0;
+int32_t previous_th1 = 0;
+int32_t previous_th2 = 0; 
 
-float shoulder_velocity = 0;
-float elbow_velocity = 0;
+int32_t th1 = 0;
+int32_t th2 = 0;
+
+float dth1 = 0;
+float dth2 = 0;
 
 float k = 0.04; //0.04;
-float d = 1;
+float d = 0;
 
-float shoulder_current = 0;
-float elbow_current = 0;
+float th1_current = 0;
+float th2_current = 0;
+
+void send_telemetry_to_computer() {
+  VescUart::dataPackage data;
+  Serial.println("Sending telemetry...");
+  for(uint16_t i=0; i < trajectory_length; i++) {
+    data = th1_telemetry[i];
+    Serial.print(i);
+    Serial.print(',');
+    Serial.print(data.tachometerAbs);
+    Serial.print(',');
+    Serial.print(data.rpm);
+    Serial.print(',');
+    Serial.print(data.inpVoltage);
+    Serial.print(',');
+    Serial.print(data.dutyCycleNow);
+
+    data = th2_telemetry[i];
+    Serial.print(i);
+    Serial.print(',');
+    Serial.print(data.tachometerAbs);
+    Serial.print(',');
+    Serial.print(data.rpm);
+    Serial.print(',');
+    Serial.print(data.inpVoltage);
+    Serial.print(',');
+    Serial.println(data.dutyCycleNow);
+  }
+  Serial.println("Telemetry sent.");
+}
 
 
 void setup() {
@@ -40,8 +71,8 @@ void setup() {
   while (!Serial) {;}
 
   // Define which ports to use as UART
-  shoulder.setSerialPort(&Serial1);
-  elbow.setSerialPort(&Serial2);
+  th1_vesc.setSerialPort(&Serial1);
+  th2_vesc.setSerialPort(&Serial2);
 }
 
 void loop() {
@@ -49,124 +80,56 @@ void loop() {
   // send trajectory, obtain telemetry
   Serial.println("Executing trajectory...");
   for(uint16_t i=0; i < trajectory_length; i++) {
-    shoulder.getVescValues();
-    elbow.getVescValues();
+    th1_vesc.getVescValues();
+    th2_vesc.getVescValues();
 
-    shoulder_telemetry[i] = shoulder.data;
-    elbow_telemetry[i] = elbow.data;
+    th1_telemetry[i] = th1_vesc.data;
+    th2_telemetry[i] = th2_vesc.data;
 
-    shoulder.setCurrent(14*shoulder_trajectory[i]);
-    elbow.setCurrent(-14*elbow_trajectory[i]);
+    th1_vesc.setCurrent(th1_trajectory[i]);
+    th2_vesc.setCurrent(th2_trajectory[i]);
     delay(10);
   }
-
-  // hey babe, send telemetry ;)
-  VescUart::dataPackage data; 
-  Serial.println("Trajectory complete. Dumping data...");
-  for(uint16_t i=0; i < trajectory_length; i++) {
-    data = shoulder_telemetry[i];
-    Serial.print(i);
-    Serial.print(',');
-    Serial.print(data.tachometerAbs);
-    Serial.print(',');
-    Serial.print(data.rpm);
-    Serial.print(',');
-    Serial.print(data.inpVoltage);
-    Serial.print(',');
-    Serial.print(data.dutyCycleNow);
-
-    data = elbow_telemetry[i];
-    Serial.print(i);
-    Serial.print(',');
-    Serial.print(data.tachometerAbs);
-    Serial.print(',');
-    Serial.print(data.rpm);
-    Serial.print(',');
-    Serial.print(data.inpVoltage);
-    Serial.print(',');
-    Serial.println(data.dutyCycleNow);
-  }
-
-  Serial.println("Trajectroy sent. Breaking.");
   */
 
-  shoulder.getVescValues();
-  elbow.getVescValues();
-  initial_shoulder_angle = shoulder.data.tachometer;
-  initial_elbow_angle = elbow.data.tachometer;
-  last_shoulder_angle = initial_shoulder_angle;
-  last_elbow_angle = initial_elbow_angle; 
-
-  current_shoulder_angle = initial_shoulder_angle;
-  current_elbow_angle = initial_elbow_angle;
-  //for(uint16_t i=0; i < 300; i++){
   while(1){
     // log telemetry
-    shoulder.getVescValues();
-    elbow.getVescValues();
-
-    //shoulder_telemetry[i] = shoulder.data;
-    //elbow_telemetry[i] = elbow.data;
+    th1_vesc.getVescValues();
+    th2_vesc.getVescValues();
     
     // get positions and velocities
-    current_shoulder_angle = shoulder_encoder.read();
-    current_elbow_angle = elbow_encoder.read();
+    th1 = th1_encoder.read();
+    th2 = th2_encoder.read();
     
-    shoulder_velocity = 0.01*(current_shoulder_angle - last_shoulder_angle);
-    elbow_velocity    = 0.01*(current_elbow_angle - last_elbow_angle);
+    dth1 = 0.01*(th1 - previous_th1);
+    dth2 = 0.01*(th2 - previous_th2);
 
-    last_shoulder_angle = current_shoulder_angle;
-    last_elbow_angle = current_elbow_angle;
+    previous_th1 = th1;
+    previous_th2 = th2;
 
     // make impedance controller
-    shoulder_current = k*(initial_shoulder_angle - current_shoulder_angle) + d*(-shoulder_velocity);
-    elbow_current = k*(initial_elbow_angle - current_elbow_angle) + d*(-elbow_velocity);
+    th1_current = k*(th1_desired - th1) + d*(-dth1);
+    th2_current = k*(th2_desired - th2) + d*(-dth2);
 
-    if(shoulder_current > 70) shoulder_current = 70;
-    if(shoulder_current < -70) shoulder_current = -70;
-    if(elbow_current > 70) elbow_current = 70;
-    if(elbow_current < -70) elbow_current = -70;
+    if(th1_current >  60) th1_current =  60;
+    if(th1_current < -60) th1_current = -60;
+    if(th2_current > 60)  th2_current =  60;
+    if(th2_current < -60) th2_current = -60;
 
-    shoulder.setCurrent(-shoulder_current);    
-    elbow.setCurrent(-elbow_current);
-    Serial.print(initial_shoulder_angle);
+    th1_vesc.setCurrent(-th1_current);    
+    th2_vesc.setCurrent(-th2_current);
+
+    Serial.print("th1:");
+    Serial.print(th1);
     Serial.print(",");
-    Serial.print(current_shoulder_angle);
+
+    Serial.print("dth1:");
+    Serial.print(dth1);
     Serial.print(",");
-    Serial.print(shoulder_velocity);
-    Serial.print(",");
-    Serial.print(shoulder_current);
-    Serial.print(",");
-    Serial.println(shoulder.data.avgMotorCurrent);
+
+    Serial.print("th1_current:");
+    Serial.println(th1_current);
     delay(10);
   }
   while(1);
-
-  /*
-  VescUart::dataPackage data;
-  Serial.println("Impedance control run complete. Dumping data...");
-  for(uint16_t i=0; i < trajectory_length; i++) {
-    data = shoulder_telemetry[i];
-    Serial.print(i);
-    Serial.print(',');
-    Serial.print(data.tachometerAbs);
-    Serial.print(',');
-    Serial.print(data.rpm);
-    Serial.print(',');
-    Serial.print(data.inpVoltage);
-    Serial.print(',');
-    Serial.print(data.dutyCycleNow);
-
-    data = elbow_telemetry[i];
-    Serial.print(i);
-    Serial.print(',');
-    Serial.print(data.tachometerAbs);
-    Serial.print(',');
-    Serial.print(data.rpm);
-    Serial.print(',');
-    Serial.print(data.inpVoltage);
-    Serial.print(',');
-    Serial.println(data.dutyCycleNow);
-  }
-  */
 }
